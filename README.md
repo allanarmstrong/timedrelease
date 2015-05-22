7c1ed01d2f4ce7f363be959d3a0124aca268ee8a
Allan Armstrong <allan@hit3k.net>
2015-05-22 15:12:24 +1000

    Removed debug code from plugin and added more to README.md

--- README.md
+++ README.md
@@ -15,7 +15,10 @@ Tags
 
 <strong>Example</strong>  
 [timed-release date="1-1-2016" time="00:01"]Happy New Year![/timed-release]  
-This will show "Happy New Year!" at 12:01AM on the 1/1/2016.
+This will show "Happy New Year!" at 12:01AM on the 1/1/2016.  
+  
+The format for the date is <em>"dd-mm-yyyy"</em> or a variation thereof.  
+The format for the time is <em>"HH:MM"</em> in 24 hour notation (Hours from 0-23)
 
 Not specifying a date or time will cause the tag to default to 24 hours of the time the post was made.
 
--- timedrelease.php
+++ timedrelease.php
@@ -13,20 +13,22 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 defined( 'ABSPATH' ) or die ('no script kiddies please');
 
 function timed_release_shortcode($atts , $content = null) {
-	
-	$defaulttime = time() + 86400;
+	$gmt_offset = get_option('gmt_offset');
+	$currentTime = time() + ($gmt_offset * 60 * 60);
+	$defaulttime = (int)strtotime(get_the_date()) + 86400;
 
 	$a = shortcode_atts(array(
-		'date' => $defaulttime, //24 hours from now
-		'time' => '00:00' //Midnight
+		'date' => $defaulttime, // Defaults to 24 hours from now
+		'time' => '00:00' //Midnight. Doesn't affect default release time.
 	), $atts);
-	if ($a['date'] != $timestamp) {
+	if ($a['date'] != $defaulttime) {
 		$date = explode('-', $a['date']);
 		$time = explode(':', $a['time']);
 		$timestamp = mktime((int)$time[0],(int)$time[1],0,(int)$date[1], (int)$date[0], (int)$date[2]);
+	} else {
+		$timestamp = $a['date'];
 	}
-
-	if ((int)time() < (int)$timestamp) {
+	if ( $currentTime < (int)$timestamp) {
 		return '';
 	} else {
 		return '<p>' . do_shortcode($content) . '</p>';
